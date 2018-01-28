993cf3190813dde5f15f69aed6fb7185fc376be4
Tim Denison <tim.denison@gmail.com>
2018-01-28 08:53:57 -0800

    adds firebase with config

---
+++ src/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+var config = {
+    apiKey: "AIzaSyDl5Iahc0gfcdJ_OycFw3cFnPA6vK2GGDI",
+    authDomain: "karaoke-6876e.firebaseapp.com",
+    databaseURL: "https://karaoke-6876e.firebaseio.com",
+    projectId: "karaoke-6876e",
+    storageBucket: "karaoke-6876e.appspot.com",
+    messagingSenderId: "603772681466"
+  };
+var fire = firebase.initializeApp(config);
+export default fire;
